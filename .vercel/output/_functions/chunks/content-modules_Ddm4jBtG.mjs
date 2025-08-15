641d893b7526caf2342271ce91f5a43e9fb5675d
AMAIDI MOHAMED <amaidim30@gmail.com>
2025-08-15 10:02:36 +0100

    Delete .vercel/output directory

--- .vercel/output/_functions/chunks/content-modules_Ddm4jBtG.mjs
+++
@@ -1,8 +0,0 @@
-const contentModules = new Map([
-["src/content/articles/transforming-raw-data-into-actionable-insights.mdoc", () => import('./transforming-raw-data-into-actionable-insights_CVbaPC4w.mjs')],
-["src/content/articles/unlocking-the-power-of-predictive-analytics.mdoc", () => import('./unlocking-the-power-of-predictive-analytics_BY_wTHUK.mjs')],
-["src/content/reference/cybersecurity-finland-advanced-security-reference.mdoc", () => import('./cybersecurity-finland-advanced-security-reference_8f0wK7kW.mjs')],
-["src/content/reference/cybersecurity-finland-quick-start-guide.mdoc", () => import('./cybersecurity-finland-quick-start-guide_C6laWxCd.mjs')],
-["src/content/reference/cybersecurity-finland-user-manual.mdoc", () => import('./cybersecurity-finland-user-manual_caKG3FN5.mjs')]]);
-
-export { contentModules as default };
--- .vercel/output/_functions/chunks/cybersecurity-finland-advanced-security-reference_8f0wK7kW.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./cybersecurity-finland-advanced-security-reference_CygtUQuN.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
